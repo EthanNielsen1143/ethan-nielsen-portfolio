7e1d0ccc3465de1d9723c1892b6afe0e536e151f
Ethan Nielsen <ecnielsen11@byui.edu>
2025-04-02 17:44:09 -0600

    mobile view added, unfinished but working

---
+++ src/pages/HomeMobile.tsx
@@ -0,0 +1,74 @@
+import { Heading, Text, Flex, Image, Button } from "@chakra-ui/react";
+import TypewriterText from "@/components/TypeWriterText";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+function HomeMobile() {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowButton(true);
+    }, 5300);
+
+    return () => clearTimeout(timer);
+  });
+
+  function downloadResume(): void {
+    const link = document.createElement("a");
+    link.href = "/files/Ethan_Nielsen_Resume.pdf";
+    link.download = "Ethan_Nielsen_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+  return (
+    <Flex
+      height="100vh"
+      width="100vw"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      textAlign="center"
+      gap="6vh"
+      padding="6vw"
+    >
+      <Heading fontSize="6vw">Ah! I see you are on your mobile device!</Heading>
+      <TypewriterText
+        fontSize="3.8vw"
+        text="This resume site is probably the coolest site you will ever see. However, this adventure is meant for desktops. 
+      Here is the download link in-case you want to checkout my resume now."
+      />
+      <AnimatePresence>
+        {showButton && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.9 }}
+            transition={{ duration: 0.4 }}
+          >
+            <Button
+              padding="7vw"
+              fontSize="4vw"
+              borderRadius="10px"
+              width="50vw"
+              colorPalette="purple"
+              variant="outline"
+              onClick={downloadResume}
+            >
+              Download Resume
+            </Button>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <TypewriterText
+        fontSize="3.8vw"
+        delay={6}
+        text="I would suggest checking this same site on your machine... unless you don't know how to fly a spaceship. Then maybe don't."
+      />
+    </Flex>
+  );
+}
+
+export default HomeMobile;
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { Box, useMediaQuery } from "@chakra-ui/react";
 import Nav from "../components/Nav";
 import { useEffect } from "react";
 
 const Root = () => {
+  const [isMobile] = useMediaQuery(["(max-width: 768px)"], {
+    ssr: false,
+    fallback: [false],
+  });
+
   useEffect(() => {
     const videoElement = document.getElementById(
       "backgroundVideo"
@@ -12,33 +17,35 @@ const Root = () => {
       videoElement.playbackRate = 0.75;
     }
   }, []);
+
+  if (isMobile) {
+    return <Outlet />;
+  }
+
   return (
-    <>
-      <Box pos="relative" width="100%" height="100vh" overflow="hidden">
-        <video
-          id="backgroundVideo"
-          autoPlay
-          loop
-          muted
-          playsInline
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            zIndex: -1,
-          }}
-        >
-          <source src="../videos/27770-365891067.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <Nav />
-        <Outlet />
-        {/* <Footer /> */}
-      </Box>
-    </>
+    <Box pos="relative" width="100%" height="100vh" overflow="hidden">
+      <video
+        id="backgroundVideo"
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: -1,
+        }}
+      >
+        <source src="../videos/27770-365891067.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <Nav />
+      <Outlet />
+    </Box>
   );
 };
 
